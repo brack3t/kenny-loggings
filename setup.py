a6c87c70207d650d4c65cfac9d303061868d2ef2
Chris Jones <chris@brack3t.com>
2013-01-09 16:24:24 -0800

    better setup.py

--- setup.py
+++ setup.py
@@ -5,23 +5,21 @@ setup(
     version="0.1.0",
     description='',
     long_description='',
-    keywords="python, django",
+    keywords="python, django, logging",
     author="Chris Jones <chris@brack3t.com>",
     author_email="chris@brack3t.com",
     url="https://github.com/brack3t/kenny-loggings",
     license="BSD",
     packages=["loggings"],
     zip_safe=False,
-    install_requires=["requests"],
     include_package_data=True,
     classifiers=[
-        "Programming Language :: Python :: 2 :: Only",
+        "Programming Language :: Python",
+        "Framework :: Django",
         "License :: OSI Approved :: BSD License",
-        "Topic :: Software Development :: Libraries :: Python Modules",
         "Environment :: Web Environment",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Topic :: Internet",
-        "Intended Audience :: Information Technology"
+        "Intended Audience :: Developers"
     ],
 )
