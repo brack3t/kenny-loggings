df86c8026d9da034d474b3b3b3a4160186d544b2
Chris Jones <chris@brack3t.com>
2013-01-23 13:08:06 -0800

    Bump version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="kenny-loggings",
-    version="0.1.1",
+    version="0.1.2",
     description="A simple Django model event logger.",
     long_description="A simple Django model event logger.",
     keywords="python, django, logging",
